41a64c4b35a6305cb7b9ed26254477de6a8763ae
Varunhrdcr <117508048+Varunhrdcr@users.noreply.github.com>
2024-01-23 01:25:47 +0530

    module testing

--- server/scripts/deploy.js
+++ server/scripts/deploy.js
@@ -1,22 +1,13 @@
-const main = async () => {
-  const contractFactory = await ethers.getContractFactory('DeTweetContract');
+const hre = require("hardhat");
 
-  // Deploy the contract once:
-  const contract = await contractFactory.deploy();
+async function main() {
+  const chai = await hre.ethers.getContractFactory("DeTweetContract");
+  const contract = await chai.deploy(); //instance of contract
 
-  // Log the deployed contract's address:
-  console.log("Contract deployed to: ", contract.address);
-};
-
-
-const runMain = async() => {
-  try {
-    await main();
-    process.exit(0);
-  } catch(error) {
-    console.log(error);
-    process.exit(1);
-  }
+  await contract.deployed();
+  console.log("Contract Deployed at : ", contract.address);
 }
-
-runMain();
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
